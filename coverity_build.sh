0f29b4241917f8811830f475aa701b27f177bb3d
cgzones <cgzones@googlemail.com>
2015-02-09 15:36:40 +0100

    fix .travis.yml file

---
+++ coverity_build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# This script runs all compilations for a coverity build
+
+set -e
+
+if [ "${COVERITY}" != "yes" ]; then exit 0; fi
+
+cd src/
+
+make clean
+make TARGET=server USE_ZEROMQ=yes USE_GEOIP=yes USE_PRELUDE=yes USE_OPENSSL=auto USE_INOTIFY=yes V=1 build -j
+
+make clean
+make TARGET=agent USE_OPENSSL=auto V=1 build -j
+
+make clean
+make TARGET=winagent V=1 build -j
---
+++ coverity_prepend.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# This script sets up the environment for a coverity build
+
+set -e
+
+if [ "${COVERITY}" != "yes" ]; then exit 0; fi
+
+sudo apt-get update -qq
+sudo apt-get install geoip-bin geoip-database libgeoip-dev libgeoip1 libprelude-dev libzmq3-dev aptitude
+
+wget http://download.zeromq.org/czmq-2.2.0.tar.gz
+tar xfz czmq-2.2.0.tar.gz
+cd czmq-2.2.0/
+./configure
+make all -j
+sudo make install
+
+sudo aptitude -y install mingw-w64 nsis
+
+cd ../
